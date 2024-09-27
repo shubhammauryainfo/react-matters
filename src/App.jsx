a387bc6a1384445a0c32b051145c4a65ac3816b6
shubhammauryainfo <shubhammaurya8288@gmail.com>
2024-09-27 13:37:57 +0530

    uishaden

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
- <div>Shubham</div>
+ <div>
+  <Button>Subscribe</Button>
+ </div>
     </>
   )
 }
